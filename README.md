a6aa4d4d534469d71a42193c17dc1c0d5f898eaa
Malcolm Yam <malcolmyam38@gmail.com>
2020-08-18 20:27:18 +0800

    updated with psi api content and css changes

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # psi-spy
-Simple PSI app
+
+1. Initiate project on Github
+2. Clone into vscode
+3. Create base folders and skeleton html
+4. Get bootstrap in
+5. Get jquery and popper in 
+6. Add in base css and js scripts
+7. Get starter skeleton template from Bootstrap
+8. Create basic UI with coloumns and setup the IDs
+9. Start querying API for details
+10. Create a logo 
+   1. https://hatchful.shopify.com/
+   2. https://www.wix.com/logo/maker
+   3. https://www.namecheap.com/logo-maker/
+11. 
+
---
+++ js/main.js
@@ -0,0 +1,86 @@
+/*
+date_time
+string
+(query)	
+YYYY-MM-DD[T]HH:mm:ss (SGT)
+
+date
+string
+(query)
+YYYY-MM-DD
+
+
+*/
+$(document).ready(function () {
+  //attaching on click event handler to the id #query-btn
+
+  //let enterDate = $("#dateSelect").val(); //NOT !value()
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth();
+  let yyyy = today.getFullYear();  
+  if(dd < 10 ){
+      dd = '0' + dd;
+  }
+  if(mm < 10){
+      mm = '0' + mm;
+  }
+
+  let dateQuery = `${yyyy}-${mm}-${dd}`;
+  $.ajax({
+    type: "GET", //POST | PUT | DELETE
+    dataType: "json",
+    contentType: "text/plain",
+    url: `https://api.data.gov.sg/v1/environment/psi?date=${dateQuery}`,
+    success: function (data) {
+        console.log(data);
+
+        let psi_twenty_four_hourly = data.items[0].readings.psi_twenty_four_hourly;
+        console.log(psi_twenty_four_hourly);
+        let psi_twenty_four_hourly_national = data.items[0].readings.psi_twenty_four_hourly.national;
+        console.log(psi_twenty_four_hourly_national);
+
+        //color updates using css classes
+        //based on a certain function
+        let quality = getPSIColor(psi_twenty_four_hourly_national);
+        console.log("Quality: " + quality);
+        $(".latest-psi-status").addClass(`psi-${quality}`);
+        $(".latest-psi-state").addClass(`psi-${quality}`);
+        $(".stats-placeholder").addClass(`psi-${quality}`);
+        //number updates to the content
+        //simply updating here
+        $('.latest-psi-state').html(psi_twenty_four_hourly_national);
+      /*
+          let target10 = data.items[0].readings.pm10_twenty_four_hourly;
+          let target25 = data.items[0].readings.pm25_twenty_four_hourly;
+            
+          $.each(target10, function(key, value){
+            // console.log(key + ":" + value);
+            $('#todo-list1').append(`${key}: <b>${value}</b><br/>`);
+          });
+          $.each(target25, function(key, value){
+            // console.log(key + ":" + value);
+            $('#todo-list2').append(`${key}: <b>${value}</b><br/>`);
+          });
+          */
+    },
+  });//end of ajax call
+
+  function getPSIColor(psiValue){
+    if(psiValue >= 0 && psiValue <= 50){
+        //good
+        return "good";
+    }else if(psiValue >= 51 && psiValue <= 100){
+        return "moderate";
+    }else if(psiValue >= 101 && psiValue <= 200){
+        return "unhealthy";
+    }else if(psiValue >= 201 && psiValue <= 300){
+        return "very-unhealthy";
+    }else if(psiValue > 300){
+        return "hazardous";
+    }else{
+        console.log("invalid psi value");
+    }
+    return -1;
+  }
+});
